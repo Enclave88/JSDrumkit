03a193c25ab11a09f59656c43c4d06af55966a3b
Sutangu <sutangu@gmail.com>
2018-02-04 12:29:30 +0600

    functions changes to arrow f

--- play.js
+++ play.js
@@ -1,9 +1,9 @@
-function removeTransition(e) {
+removeTransition = (e) => {
     if(e.propertyName !== 'transform') return;
-    this.classList.remove('playing');
-}
+    e.currentTarget.classList.remove('playing');
+};
 
-function playSound(e) {
+playSound = (e) => {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if (!audio) return;
@@ -12,7 +12,7 @@ function playSound(e) {
     audio.currentTime = 0;
     audio.load();
     audio.play();
-}
+};
 
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
